01d174114ed55da0a64894856b6f010e090c8ca6
Automation <35348707+ritamganguli@users.noreply.github.com>
2024-06-10 20:42:06 +0530

    push

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -29,7 +29,7 @@ public class Test4
         String platformName = System.getenv("HYPEREXECUTE_PLATFORM") != null ? System.getenv("HYPEREXECUTE_PLATFORM") : platform;
         
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("build", "[HyperExecute - 4] Demonstration of the TestNG Framework");
+        capabilities.setCapability("build", "HyperExecute -Demonstration of the TestNG Framework");
         capabilities.setCapability("name", "[HyperExecute - 4] Demonstration of the TestNG Framework");
 
         capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
