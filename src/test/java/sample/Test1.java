ca36a9ad12ea095387ff557d8ffe0c2710cb3d5c
Umesh Kumar <umeshk@LT-MBP-090.local>
2024-06-21 20:58:57 +0530

    Update for v3 yaml

--- src/test/java/sample/Test1.java
+++ src/test/java/sample/Test1.java
@@ -1,9 +1,10 @@
+package sample;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,26 +12,30 @@ import java.time.Duration;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-public class Test2
+
+public class Test1
 {
-    WebDriver driver = null;
+    RemoteWebDriver driver = null;
     public static String status = "passed";
-    String username = Test1.username;
-    String access_key = Test1.access_key;
+    public static String username = System.getenv("LT_USERNAME");
+    public static String access_key = System.getenv("LT_ACCESS_KEY");
+    public static String browser = "Chrome";
+    public static String version = "latest";
+    public static String platform = "Windows";
+    public static String resolution = "1024x768";
 
 //    String testURL = "https://todomvc.com/examples/react/#/";
     String testURL = "https://lambdatest.github.io/sample-todo-app/";
     String testURLTitle = "Sample page - lambdatest.com";
     @BeforeMethod
-    @Parameters(value={"browser","version","platform", "resolution"})
-    public void testSetUp(String browser, String version, String platform, String resolution) throws Exception
+    public void testSetUp() throws Exception
     {
         String platformName = System.getenv("HYPEREXECUTE_PLATFORM") != null ? System.getenv("HYPEREXECUTE_PLATFORM") : platform;
         
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("build", "[HyperExecute - 2] Demonstration of the TestNG Framework");
-        capabilities.setCapability("name", "[HyperExecute - 2] Demonstration of the TestNG Framework");
 
+        capabilities.setCapability("build", "[HyperExecute - 1] Demonstration of the TestNG Framework");
+        capabilities.setCapability("name", "[HyperExecute - 1] Demonstration of the TestNG Framework");
         capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
         capabilities.setCapability("browserName", browser);
         capabilities.setCapability("version", version);
@@ -52,9 +57,10 @@ public class Test2
     }
 
     @Test(description="To Do App on React App")
-    public void test2_element_addition_1() throws InterruptedException
-    {   ExtentReports extent = new ExtentReports("target/surefire-reports/html/extentReport.html");
-        ExtentTest test1 = extent.startTest("demo application test 2-1", "To Do App test 1");
+    public void test1_element_addition_1() throws InterruptedException
+    {
+        ExtentReports extent = new ExtentReports("target/surefire-reports/html/extentReport.html");
+        ExtentTest test1 = extent.startTest("demo application test 1-1", "To Do App test 1");
 
         driver.get(testURL);
         Thread.sleep(5000);
@@ -102,6 +108,63 @@ public class Test2
         }
 
         extent.endTest(test1);
+        extent.flush();
+
+        /* Once you are outside this code, the list would be empty */
+    }
+
+    @Test(description="To Do App on React App")
+    public void test1_element_addition_2() throws InterruptedException
+    {
+        ExtentReports extent = new ExtentReports("target/surefire-reports/html/extentReport.html");
+        ExtentTest test2 = extent.startTest("demo application test 1-2", "To Do App test 2");
+
+        driver.get(testURL);
+        Thread.sleep(5000);
+
+        test2.log(LogStatus.PASS, "URL is opened");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        test2.log(LogStatus.PASS, "Wait created");
+
+        By textField = By.id("sampletodotext");
+
+        WebElement addText = driver.findElement(textField);
+
+        int item_count = 5;
+
+        for (int i = 1; i <= item_count; i++) {
+            addText.click();
+            addText.sendKeys("Adding a new item " + i + Keys.ENTER);
+            test2.log(LogStatus.PASS, "New item No. " + i + " is added");
+            Thread.sleep(2000);
+        }
+
+        WebElement temp_element;
+
+        int totalCount = item_count+5;
+        int remaining = totalCount-1;
+
+        for (int i = 1; i <= totalCount; i++, remaining--) {
+
+            String xpath = "(//input[@type='checkbox'])["+i+"]";
+
+            driver.findElement(By.xpath(xpath)).click();
+            Thread.sleep(500);
+            test2.log(LogStatus.PASS, "Item No. " + i + " marked completed");
+            By remainingItem = By.className("ng-binding");
+            String actualText = driver.findElement(remainingItem).getText();
+            String expectedText = remaining+" of "+totalCount+" remaining";
+
+            if (!expectedText.equals(actualText)) {
+                test2.log(LogStatus.FAIL, "Wrong Text Description");
+                status = "failed";
+            }
+            Thread.sleep(500);
+
+            test2.log(LogStatus.PASS, "Item No. " + i + " completed");
+        }
+
+        extent.endTest(test2);
         extent.flush();
 
         /* Once you are outside this code, the list would be empty */
