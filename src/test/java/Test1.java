c55f51d13f56d63ff7f4ad12076424aa11047c98
ambarg27 <146844240+ambarg27@users.noreply.github.com>
2026-09-26 20:56:13 +0530

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -219,8 +219,8 @@ public class Test1 {
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-           // ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
-            ((JavascriptExecutor) driver).executeScript("lambda-status=" + "failed");
+           ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            // ((JavascriptExecutor) driver).executeScript("lambda-status=" + "failed");
             driver.quit();
         }
     }
